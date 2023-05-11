a378e809515e0bf14a2b38f4a519e985367458e5
Liv Cella <livncella@gmail.com>
2023-05-11 16:53:11 -0600

    rollback
    This reverts commit 0daa4151f3067b0b007aa29fc0a6a2465f634a57.

--- lib/game_of_life/board.ex
+++
@@ -1,8 +0,0 @@
-defmodule Board do
- # board state, (current iteration of all cells)
- #
-
-  def tick() do
-
-  end
-end
--- lib/game_of_life/game.ex
+++ lib/game_of_life/game.ex
@@ -2,27 +2,12 @@ defmodule GameOfLife.Game do
   @rows 5
   @cols 5
 
-  # TODO: return a struct that represents the entire board
-  # have coordinates for every point on the board
   def new() do
-    # Generate a map/struct that increments each x,y by 1 and assigns false
-    # for all but the given coordinates
     %{
-      {0, 0} => false,
-      {0, 1} => false,
-      {0, 2} => false,
-      {0, 3} => false,
-      {0, 4} => false,
-
-      {2, 2} => false,
       {2, 3} => true,
       {3, 3} => true,
       {4, 3} => true
     }
-
-    for {{x, y}, v} <- 1..5, into: %{} do
-      {x + 1, y + 1}
-    end
   end
 
   defp rule(state, 2) do
